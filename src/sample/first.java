560943ad6c2bdd41ca294dc34c93ad04015e1f55
lokesh-janghel <lokesh.janghel@accoliteindia.com>
2017-07-10 19:35:47 +0530

    Added first.java

---
+++ src/sample/first.java
@@ -0,0 +1,27 @@
+/*
+ * 
+ */
+package sample;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class first.
+ */
+public class first {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String args[]){
+		//String s = "Hello World!!!";
+		System.out.println("HEllO WORLD!!!");
+		int x=1;
+		for (int i = 0; i < 5; i++) {
+			x+=3;
+			System.out.println(x);
+		}
+	}
+	
+}
